2e082502f06e768849316aa31c8178eb1d133475
prachee-en <prachiagarwal2002july@gmail.com>
2024-09-04 02:16:33 +0530

    added more completed buttons added files

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -4,10 +4,40 @@ const ButtonsPage = () => {
     return (
 <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
     <Button>
+        Default
+    </Button>
+    <Button variant="primary">
         Primary
     </Button>
-    <Button>
+    <Button variant="primaryOutline">
         Primary outline
+    </Button>
+    <Button variant="secondary">
+        Secondary
+    </Button>
+    <Button variant="secondaryOutline">
+      secondary outline
+    </Button>
+    <Button variant="danger">
+        Danger
+    </Button>
+    <Button variant="dangerOutline">
+      Danger outline
+    </Button>
+    <Button variant="super">
+        super
+    </Button>
+    <Button variant="superOutline">
+      super outline
+    </Button>
+    <Button variant="ghost">
+      Ghost
+    </Button>
+    <Button variant="sidebar">
+        sidebar
+    </Button>
+    <Button variant="sidebarOutline">
+      sidebar outline
     </Button>
 </div>
     );
